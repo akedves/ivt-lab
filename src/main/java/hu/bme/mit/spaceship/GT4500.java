ec34a24f6137a28c1df406f963cf74089d6f2121
akedves <aron.kedves@gmail.com>
2021-04-15 19:10:31 +0200

    fix delete

--- src/main/java/hu/bme/mit/spaceship/GT4500.java
+++ src/main/java/hu/bme/mit/spaceship/GT4500.java
@@ -78,25 +78,8 @@ public class GT4500 implements SpaceShip {
 
       case ALL:
         // try to fire both of the torpedo stores
-        boolean primaryFired = false;
-        boolean secondaryFired = false;
+        //TODO
 
-        if(primaryTorpedoStore.isEmpty() && secondaryTorpedoStore.isEmpty()){}
-        else{
-          if(!primaryTorpedoStore.isEmpty()){
-            int numberOfTorpedosInPrimary = primaryTorpedoStore.getTorpedoCount();
-            primaryFired = primaryTorpedoStore.fire(numberOfTorpedosInPrimary);
-          }
-          if(!secondaryTorpedoStore.isEmpty()){
-            int numberOfTorpedosInSecondary = secondaryTorpedoStore.getTorpedoCount();
-            secondaryFired = secondaryTorpedoStore.fire(numberOfTorpedosInSecondary);
-          }
-        }
-        
-        if(primaryFired || secondaryFired){
-          firingSuccess = true;
-        }
-        
         break;
     }
 
